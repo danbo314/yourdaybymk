2d78b0544fef161e47b7613c480b2f66688f5a4b
Dan Bolivar <danbolivar@dbolivar-mbp-chicago.lan>
2017-08-03 22:37:53 -0500

    add email http posting and style fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { ContainerComponent } from './components/container/container.component';
 import { HomeComponent } from './components/main/main.component';
 import { BlogComponent } from './components/blog/blog.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SocialMediaComponent } from './components/social-media/social-media.component';
+
+declare var require: any;
 
 @NgModule({
   declarations: [
@@ -18,9 +21,11 @@ import { SocialMediaComponent } from './components/social-media/social-media.com
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [ContainerComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/services/email.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
+@Injectable()
+export class EmailService {
+    private headers = new Headers({'Content-Type': 'application/json'});
+
+    constructor(private http: Http) { }
+
+    send(param: object) {
+        return this.http.post('https://formspree.io/yourdaybymk@gmail.com', param, { headers: this.headers }).toPromise();
+    }
+}
